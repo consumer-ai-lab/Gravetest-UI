7da36b66bd24d51559019b308aa429f1eeff06d7
Aditya Bhattad <adityabhattad18@gmail.com>
2024-08-19 10:52:17 +0530

    add login page

--- src/pages/tests.tsx
+++ src/pages/tests.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 
 export default function TestsPage(){
     const {testId} = useParams();
-    console.log(testId);
+
     return (
         <div>
             Tests Page.
