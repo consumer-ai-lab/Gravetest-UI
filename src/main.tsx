a3d2104012cffb1029875cfd5068447172b3679b
Aditya Bhattad <adityabhattad18@gmail.com>
2024-08-19 10:33:04 +0530

    setup react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import InstructionsPage from './pages/instructions';
+import LoginPage from './pages/login-page';
+import TestsPage from './pages/tests';
+import EndPage from './pages/end';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <InstructionsPage/>,
+  },
+  {
+    path:"/login",
+    element:<LoginPage/>,
+  },
+  {
+    path:"/tests/:testId",
+    element:<TestsPage/>
+  },
+  {
+    path:"/end",
+    element:<EndPage/>
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
